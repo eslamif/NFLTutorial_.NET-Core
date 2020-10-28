f3744d57fa495c1b002e89bb279c5c6e8fd8fd72
Frank Eslami <frank.eslami@mbc.ca.gov>
2020-10-28 14:07:17 -0700

    NFLCookies created.

---
+++ NFLTutorial/Models/NFLCookies.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NFLTutorial.Models
+{
+    public class NFLCookies
+    {
+        private const string TEAMS_KEY = "MyTeams";
+        private const string DELIMITER = "-";
+
+        private IRequestCookieCollection RequestCookies { get; set; }
+        private IResponseCookies ResponseCookies { get; set; }
+
+        public NFLCookies(IRequestCookieCollection requestCookies)
+        {
+            this.RequestCookies = requestCookies;
+        }
+
+        public NFLCookies(IResponseCookies responseCookies)
+        {
+            this.ResponseCookies = responseCookies;
+        }
+
+        public void SetMyTeamIds(List<Team> myTeams)
+        {
+            List<string> ids = myTeams.Select(t => t.TeamID).ToList();
+            string idString = string.Join(DELIMITER, ids);
+
+            CookieOptions options = new CookieOptions
+            {
+                Expires = DateTime.Now.AddDays(30)
+            };
+
+            RemoveMyTeamIds();                                      //remove any existing cookies
+            ResponseCookies.Append(TEAMS_KEY, idString, options);   //add new cookies
+        }
+
+        public string[] GetMyTeamIds()
+        {
+            string cookies = RequestCookies[TEAMS_KEY];
+            return string.IsNullOrEmpty(cookies) ? new string[] { } : cookies.Split(DELIMITER);
+        }
+
+        public void RemoveMyTeamIds()
+        {
+            ResponseCookies.Delete(TEAMS_KEY);
+        }
+    }
+}
