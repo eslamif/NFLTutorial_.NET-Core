a8e48594a60c8b0fad2a834f5bb7259c17b6380a
Frank Eslami <frank.eslami@mbc.ca.gov>
2020-10-28 09:36:35 -0700

    Enabled sessions and cookies.

--- NFLTutorial/Startup.cs
+++ NFLTutorial/Startup.cs
@@ -21,15 +21,33 @@ namespace NFLTutorial {
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
+            
+            //Enable MVC
             services.AddControllersWithViews();
+
+            //Lowercase routing
             services.AddRouting(options => options.LowercaseUrls = true);
+
+            //Register TeamContext with DbContext
             services.AddDbContext<TeamContext>(options => options.UseSqlServer(Configuration.GetConnectionString("TeamContext")));
+
+            //Enable web browser caching
+            services.AddMemoryCache();
+
+            //Enable sessions
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromSeconds(60 * 5);     //5 minute timeout
+                options.Cookie.HttpOnly = false;
+                options.Cookie.IsEssential = false;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
             app.UseDeveloperExceptionPage();
             app.UseStaticFiles();
+            app.UseSession();       //Enable sessions
             app.UseRouting();
 
             app.UseEndpoints(endpoints => {
