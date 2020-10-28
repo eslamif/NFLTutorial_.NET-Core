a09f195ed7a2dd646aa8cd5a2be0dd974df03447
Frank Eslami <frank.eslami@mbc.ca.gov>
2020-10-28 15:50:38 -0700

    Favorites conroller created.

---
+++ NFLTutorial/Controllers/FavoritesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NFLTutorial.Models;
+
+namespace NFLTutorial.Controllers
+{
+    public class FavoritesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var session = new NFLSession(HttpContext.Session);
+
+            var model = new TeamListViewModel
+            {
+                ActiveConference = session.GetActiveConference(),
+                ActiveDivision = session.GetActiveDivision(),
+                Teams = session.GetMyTeams()
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public RedirectToActionResult Delete()
+        {
+            var session = new NFLSession(HttpContext.Session);
+            var cookies = new NFLCookies(Response.Cookies);
+
+            session.RemoveMyTeams();
+            cookies.RemoveMyTeamIds();
+
+            TempData["Message"] = $"Favorite teams deleted.";
+
+            return RedirectToAction("Index", "Home", new { ActiveConferend = session.GetActiveConference(), ActiveDivision = session.GetActiveDivision() });
+        }
+    }
+}
